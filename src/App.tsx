08a12029c298415f9832393afd0d045e42dcca52
ardeshirj <ardeshir.javaherchi@gmail.com>
2020-04-20 19:36:41 -0700

    Add location input & send button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import '@material/react-text-field/dist/text-field.min.css';
+import '@material/react-button/dist/button.min.css';
+import '@material/react-material-icon/dist/material-icon.css';
 import { Weather } from './features/weather/Weather';
 
 function App() {
--- src/features/weather/Weather.tsx
+++ src/features/weather/Weather.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import Button from '@material/react-button';
+import styles from './weather.module.css';
 
 export function Weather() {
+  const [location, setLocation] = React.useState("");
+
+  const onLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(event.target.value);
+  };
+
+  const onSend = (event: React.MouseEvent) => {
+    console.log(event);
+  }
+
   return (
     <div>
       <h1>Welcome to weather app!</h1>
+      <TextField
+        label='Location'
+        onTrailingIconSelect={() => setLocation('')}
+      ><Input
+          value={location}
+          onChange={onLocationChange} />
+      </TextField>
+      <Button className={styles.sendButton} onClick={onSend} raised>
+          Send
+      </Button>
     </div>
   )
 }
